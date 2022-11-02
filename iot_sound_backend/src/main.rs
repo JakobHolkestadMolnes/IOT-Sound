28c92d1dfc04851b168e34f96161e54ed56f444c
Jakob Holkestad Molnes <jakob@dirik.no>
2022-11-02 20:52:56 +0100

    changed library from pahp to rumqttc

--- iot_sound_backend/src/main.rs
+++ iot_sound_backend/src/main.rs
@@ -1,70 +1,48 @@
 use std::env;
 
-use paho_mqtt as mqtt;
+use rumqttc::{AsyncClient, MqttOptions, QoS};
 use dotenv;
 
-fn main() {
-    
-
-    println!("Hello, world!");
-
-    // add waitgroup
-    let wg = crossbeam::sync::WaitGroup::new();
-
-    // create task
-
-    {
-        let wg = wg.clone();
-
-        std::thread::spawn(move || {
-            listen_for_mqtt_messages();
-            drop(wg);
-        });
-    }
-
-    wg.wait();
-
-    println!("Goodbye, world!");
-}
-
-
-fn listen_for_mqtt_messages() {
-
-    // load env variables from .env file, place .env file in project root
+#[tokio::main]
+async fn main () {
     dotenv::dotenv().ok();
-    let qmtt_adress = env::var("MQTT_ADRESS").expect("MQTT_ADRESS must be set in .env file");
+    let mqtt_adress = env::var("MQTT_ADRESS").expect("MQTT_ADRESS must be set in .env file");
     let mqtt_port = env::var("MQTT_PORT").expect("MQTT_PORT must be set");
 
-    //create a link to the mqtt broker example: "tcp://env_var:1883"
-    let mqtt_broker = format!("tcp://{}:{}", qmtt_adress, mqtt_port);
-    let cli = mqtt::Client::new(mqtt_broker).unwrap_or_else(|err| {
-        panic!("{}", err);
-    });
-    let conn_opts = mqtt::ConnectOptionsBuilder::new()
-         .keep_alive_interval(std::time::Duration::from_secs(20))
-         .clean_session(true)
-         .finalize();
+   listen_for_message(mqtt_adress, mqtt_port).await;
 
-    println!("Connecting to the MQTT server...");
-    if let Err(e) = cli.connect(conn_opts) {
-        println!("Error connecting to the MQTT server:\n\t{:?}", e);
-        return;
-    }
+}
 
-    println!("Subscribing to the topic...");
-   let rx = cli.start_consuming();
-    let sub = cli.subscribe("test", 1);
-    if let Err(e) = sub {
-        println!("Error subscribing to the topic:\n\t{:?}", e);
-        return;
-    }
+/// Async function to listen for messages on the MQTT broker
+/// * `mqtt_adress`: String
+/// * `mqtt_port`: String
+async fn listen_for_message(mqtt_adress: String, mqtt_port: String) {
+    let mqtt_options = MqttOptions::new("sensor_node", mqtt_adress, mqtt_port.parse().unwrap());
+    let (mqtt_client, mut eventloop) = AsyncClient::new(mqtt_options, 10);
+    mqtt_client.subscribe("g6/sensor", QoS::AtLeastOnce).await
+    .expect("Failed to subscribe to topic");
 
-    println!("Waiting for messages...");
-    for m in rx.iter() {
-        if let Some(msg) = m {
-            //parse message payload
-            let payload = String::from_utf8_lossy(msg.payload());
-            println!("Received message:\n\t{:?}", payload);
+ 
+
+    let mut message_count = 0;
+    loop {
+        let notification = eventloop.poll().await;
+        match notification {
+            Ok(rumqttc::Event::Incoming(incoming)) => match incoming {
+                rumqttc::Incoming::Publish(publish) => {
+                    println!(
+                        "Received message: {:?}",
+                        std::str::from_utf8(&publish.payload).unwrap()
+                    );
+                    println!("Message count: {}", message_count);
+                    message_count += 1;
+                }
+                _ => {}
+            },
+            Ok(_) => {}
+            Err(e) => {
+                println!("Error: {:?}", e);
+            }
         }
     }
 }
