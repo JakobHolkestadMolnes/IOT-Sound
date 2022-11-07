21b87718a61e2167b18ff7e682a2ad3be63cd941
Jakob Holkestad Molnes <jakob@dirik.no>
2022-11-07 11:22:42 +0100

    problems for you guys lol, got api working tho

--- iot_sound_api/src/main.rs
+++ iot_sound_api/src/main.rs
@@ -1,24 +1,75 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use tokio_postgres;
+use tokio_postgres::{self, types::Timestamp};
 use dotenv;
 use std::env;
 use tokio;
 use deadpool_postgres;
+use serde_json::json;
+use serde::{Deserialize, Serialize};
+
+
+
+struct Data {
+    id: i32,
+    sound: String,
+    time: std::time::SystemTime,
+}
+
+// implement a trait for vec of data
+impl Data {
+    fn new(id: i32, sound: String, time: std::time::SystemTime) -> Data {
+        Data {
+            id,
+            sound,
+            time,
+        }
+    }
+}
+
+// implement a trait for vec of data
+impl Into<serde_json::Value> for Data {
+    fn into(self) -> serde_json::Value {
+        json!({
+            "id": self.id,
+            "sound": self.sound,
+            "time": self.time,
+        })
+    }
+}
+
+
+
 
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-async fn get_sound(db: &deadpool_postgres::Pool) -> impl Responder {
-    unimplemented!();
-    Ok(())
+async fn get_sound(pool: web::Data<deadpool_postgres::Pool>) -> impl Responder {
+    let client = pool.get().await.unwrap();
+    let statement = client.prepare("SELECT * FROM sound").await.unwrap();
+    let rows = client.query(&statement, &[]).await.unwrap();
+    let mut data = Vec::new();
+    for row in rows {
+        data.push(Data {
+            id: row.get(0),
+            sound: row.get(1),
+            time: row.get(2),
+        });
+    }
+
+   let data = data.into_iter().map(|data| data.into()).collect::<Vec<serde_json::Value>>();
+
+    HttpResponse::Ok().json(data)
+
+
+    
 
 }
 
 
 
 #[actix_web::main]
-async fn main() {
+async fn main() -> std::io::Result<()> {
 
 
     if  env::var("DB_USER").is_err()
@@ -42,26 +93,18 @@ async fn main() {
         ..Default::default()
     };
 
-    let manager = config .create_pool(None, tokio_postgres::NoTls).unwrap();
+    let pool = config .create_pool(None, tokio_postgres::NoTls).unwrap();
 
    
 
-    HttpServer::new(move || 
+    HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(manager.clone()))
-            .route("/", web::get().to(index))       
+            .app_data(web::Data::new(pool.clone()))
+            .route("/", web::get().to(index))
+            .route("/sound", web::get().to(get_sound))
 
-    )
-        .bind("localhost:8080")
-        .unwrap_or_else(
-            |e| panic!("Failed to bind to address: {}", e)
-        )
-        .run().await.unwrap_or_else(
-            |e| panic!("Failed to run server: {}", e)
-        );
-
-    println!("Web server started");
-
-
+ } )
+        .bind("localhost:8080")?
+        .run().await
         
 }
