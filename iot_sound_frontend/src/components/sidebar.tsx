6d040debdee94f771dbae684969552b9bbb25cb8
Kacper Lukasz Nowicki <kacperln@ntnu.no>
2022-11-14 10:41:57 +0100

    style: :lipstick: change title and data icon

--- iot_sound_frontend/src/components/sidebar.tsx
+++ iot_sound_frontend/src/components/sidebar.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { AiOutlineHome } from "react-icons/ai";
-import { IoShareSocialSharp } from "react-icons/io5";
+import { IoAnalytics } from "react-icons/io5";
 
 const sidebar = () => {
 
@@ -8,12 +8,12 @@ const sidebar = () => {
         <div className="h-screen w-48  bg-gray-700 container">
             
             <div>
-                <h1 className="text-white text-4xl p-6">Dirik</h1>
+                <h1 className="text-white text-4xl p-6">IoT Sound</h1>
             </div>
             <div className="flex flex-col pl-8 text-2xl  text-white ">
 
                 <Link draggable="false" className="buttonGradiant mt-1 p-2 pr-0 hover:translate-x-1 bg-gray-600  hover:text-red-400 nodrag" to="/"><AiOutlineHome /> Sensors</Link>
-                <Link draggable="false" className="buttonGradiant mt-1 p-2 pr-0 hover:translate-x-1 bg-gray-600  hover:text-red-400 nodrag" to="/socials"><IoShareSocialSharp /> Data</Link>
+                <Link draggable="false" className="buttonGradiant mt-1 p-2 pr-0 hover:translate-x-1 bg-gray-600  hover:text-red-400 nodrag" to="/socials"><IoAnalytics /> Data</Link>
                 </div>
 
             <div className="absolute bottom-0 left-0">
