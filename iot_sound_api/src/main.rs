6bc9edfc2f9502e76da843573e679135fa488da5
Jakob Holkestad Molnes <jakob@dirik.no>
2022-11-25 12:00:29 +0100

    feat: :sparkles: api index now show all endpoints, + added docs

--- iot_sound_api/src/main.rs
+++ iot_sound_api/src/main.rs
@@ -3,10 +3,66 @@ use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use serde::Deserialize;
 use std::env;
 
+/// Api endpoint index
+/// Shows all available endpoints
 async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    let base_url = "http://localhost:8081/";
+    let mut end_points = String::new();
+    end_points.push_str("<div> <h1>Endpoints:</h1> \r");
+    end_points.push_str("GET \r <br>");
+
+    end_points.push_str(&get_link_string(base_url, "sound"));
+    end_points.push_str(&get_link_string(base_url, "sensors"));
+    end_points.push_str(&get_link_string(base_url, "sound/sorted"));
+    end_points.push_str(&get_link_string(
+        base_url,
+        "sound/sorted/limit?limit_amount=10",
+    ));
+    end_points.push_str(&get_link_string(base_url, "logs"));
+    end_points.push_str(&get_link_string(base_url, "logs/limit?limit_amount=10"));
+
+    end_points.push_str("</div>");
+
+    // return as html
+    HttpResponse::Ok().body(end_points)
 }
 
+/// Returns a link string for the given endpoint
+/// # Arguments
+/// * `base_url` - The base url of the server
+/// * `endpoint` - The endpoint to link to
+/// # Returns
+/// * `String` - The link string
+/// # Example
+/// ```rust
+/// let base_url = "http://localhost:8081/";
+/// let endpoint = "sound";
+/// let link_string = get_link_string(base_url, endpoint);
+/// assert_eq!(link_string, "<a href=\"http://localhost:8081/sound\">sound</a> \r <br>");
+/// ```
+fn get_link_string(base_url: &str, end_point: &str) -> String {
+    let html_link_template = format!(
+        "<a href=\"{}{}\">{}</a><br>",
+        base_url, end_point, end_point
+    );
+    html_link_template
+}
+
+/// This API Endpoint is used to get the loudness of a sensor
+/// # Arguments
+/// * `pool` - The database pool
+/// # Returns
+/// * ìmpl Responder` - The loudness of the sensor
+/// # Example request
+/// ```bash
+/// curl -X GET "http://localhost:8080/sensor/1/loudness" -H "accept: application/json"
+/// ```
+/// # Example response
+/// ```json
+/// {
+///  "loudness": 0.0,
+///  "timestamp": "2020-05-01T12:00:00Z"
+/// }
 async fn get_sound(pool: web::Data<iot_sound_database::Pool>) -> impl Responder {
     let returned = pool.get_loudness().await;
     let returned = match returned {
@@ -28,6 +84,32 @@ struct Info {
     limit_amount: i64,
 }
 
+/// his api call gets the latest data from the database sorted by sensor but limited by the amount
+/// specified in the url
+/// example: /api/sound/limit?limit_amount=10
+/// # Arguments
+/// * `pool` - The database pool
+/// * `info` - The limit amount
+/// # Returns
+/// * `impl Responder` - The response
+/// # Errors
+/// * `InternalServerError` - If there is an error with the database
+/// * `NotFound` - If there is no data in the database
+/// * `BadRequest` - If the limit amount is less than 1
+/// # Example call
+/// ```bash
+/// curl -X GET "http://localhost:8080/sound/limit?limit_amount=1" -H "accept: application/json"
+/// ```
+/// # Example response
+/// ```json
+/// [
+///  {
+///   "sensor_id": "sensor1",
+///   "db_level": "50",
+///   "timestamp": "2020-05-06T12:00:00Z"
+///  },
+/// ]
+/// ```
 async fn get_sound_sorted_by_sensor_limited(
     pool: web::Data<iot_sound_database::Pool>,
     info: web::Query<Info>,
@@ -72,6 +154,45 @@ async fn get_sound_sorted_by_sensor_limited(
     }
 }
 
+/// this api call gets the latest data from the database sorted by sensor
+/// and returns it as a json object
+/// # Arguments
+/// * `pool` - the database pool
+/// # Returns
+/// * `impl Responder` - the response
+/// # Example Call
+/// ```bash
+/// curl -X GET "http://localhost:8080/sound/sorted_by_sensor" -H "accept: application/json"
+/// ```
+/// # Example Response
+/// ```json
+/// [
+///  [
+///    {
+///      "sensor_id": "sensor1",
+///      "loudness": 0,
+///      "date_time": "2021-05-03T12:00:00Z"
+///    },
+///    {
+///      "sensor_id": "sensor1",
+///      "loudness": 0,
+///      "date_time": "2021-05-03T12:00:00Z"
+///    }
+///  ],
+///  [
+///    {
+///      "sensor_id": "sensor2",
+///      "loudness": 0,
+///      "date_time": "2021-05-03T12:00:00Z"
+///    },
+///    {
+///      "sensor_id": "sensor2",
+///      "loudness": 0,
+///      "date_time": "2021-05-03T12:00:00Z"
+///    }
+///  ]
+///]
+/// ```
 async fn get_sound_sorted_by_sensor(pool: web::Data<iot_sound_database::Pool>) -> impl Responder {
     let sensors = pool.get_sensor_ids().await;
     let sensors = match sensors {
@@ -120,6 +241,11 @@ async fn get_sound_sorted_by_sensor(pool: web::Data<iot_sound_database::Pool>) -
     }
 }
 
+/// the api call that returns all sensors stored in the database
+/// # Arguments
+/// * `pool` - the database pool
+/// # Returns
+/// * `impl Responder` - the response
 async fn get_sensors(pool: web::Data<iot_sound_database::Pool>) -> impl Responder {
     let returned = pool.get_sensors().await;
     let returned = match returned {
@@ -136,6 +262,11 @@ async fn get_sensors(pool: web::Data<iot_sound_database::Pool>) -> impl Responde
     }
 }
 
+/// This is the api call to get logged errors from the database
+/// # Arguments
+/// * `pool` - The database pool
+/// # Returns
+/// * `impl Responder` - The response to the api call
 async fn get_logs(pool: web::Data<iot_sound_database::Pool>) -> impl Responder {
     let returned = pool.get_logs().await;
     let returned = match returned {
@@ -152,6 +283,12 @@ async fn get_logs(pool: web::Data<iot_sound_database::Pool>) -> impl Responder {
     }
 }
 
+/// This is the api call to get a limited amount of logged errors from the database
+/// # Arguments
+/// * `pool` - The database pool
+/// * `info` - The limit amount
+/// # Returns
+/// * `impl Responder` - The response to the api call
 async fn get_logs_limited(
     pool: web::Data<iot_sound_database::Pool>,
     info: web::Query<Info>,
@@ -171,6 +308,9 @@ async fn get_logs_limited(
     }
 }
 
+/// the main function
+/// # Returns
+/// * `Result<(), std::io::Error>` - The result of the main function
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     if env::var("DB_USER").is_err()
